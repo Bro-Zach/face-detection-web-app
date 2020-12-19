c81ffe9dc447fb8fc34e40d1943c36b5fad3f194
Bro-Zach <olatechlove@gmail.com>
2020-12-19 13:33:04 +0100

    detects face from camera

---
+++ src/FaceDetectionApp/OpenCvFaceDetectionApp/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace OpenCvFaceDetectionApp
+{
+    public static class Extensions
+    {
+        public static string ToBase64String(this Mat mat) =>
+            Convert.ToBase64String(mat.ToBytes());
+    }
+}
