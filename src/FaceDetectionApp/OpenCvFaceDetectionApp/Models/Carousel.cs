aeba1ce9ebe17942a2236046f228c47893ef660f
Bro-Zach <olatechlove@gmail.com>
2020-12-19 21:40:02 +0100

    face detection from image

---
+++ src/FaceDetectionApp/OpenCvFaceDetectionApp/Models/Carousel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace OpenCvFaceDetectionApp.Models
+{
+    public class Carousel
+    {
+        public string Id { get; set; }
+        public List<CarouselItem> Items { get; set; }
+
+
+    }
+
+    public class CarouselItem
+    {
+        public string ImageSource { get; set; }
+        public string Label { get; set; }
+        public string Description { get; set; }
+    }
+}
